a99319fc29de1f184fbd07fe612a588d93e8472c
Victor Marroquin <113811742+FEM12@users.noreply.github.com>
2022-11-07 00:24:17 -0600

    Modify 2

--- Proyecto Final/Js/Registro_Principal.js
+++ Proyecto Final/Js/Registro_Principal.js
@@ -5,19 +5,8 @@ document.getElementById("guardar").addEventListener("click", function(){
   const author = document.getElementById("Author").value;
   const gen = document.getElementById("Gen").value;
 
-  const libros = {
-
-    nombre:name,
-    autor:author,
-    genero:gen,
-
-  }
-
-  for (const i in libros) {
-    
-    console.log(libros[i]);
-
-  }
+ 
+  
 
 
 })
